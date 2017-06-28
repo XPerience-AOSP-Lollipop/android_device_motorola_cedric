29a33708a3d7f271ba3e685ccce60d600fae2909
wzedlare <vedatak01@gmail.com>
2017-06-28 14:18:42 +0000

    Revert "cedric: Disable FM Radio"
    This reverts commit b9cf326cf951581b7fe90364eb64fe377010280f.

--- device.mk
+++ device.mk
@@ -158,6 +158,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fingerprintd
 
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    libfmjni \
+    fmhalservice \
+    libqcomfm_jni
+
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
